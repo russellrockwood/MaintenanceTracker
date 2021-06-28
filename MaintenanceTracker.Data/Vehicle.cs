998c3c66090234a0e28f2c2052d28a00a5aab34b
Russell Rockwood <russell.rockwood@gmail.com>
2021-06-28 13:10:30 -0500

    updated identity models, added vehiclce dbo

---
+++ MaintenanceTracker.Data/Vehicle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaintenanceTracker.Data
+{
+    public class Vehicle
+    {
+        [Key]
+        public int VehicleId { get; set; }
+
+        [Required]
+        [Range(2,4,ErrorMessage ="Example: 2018 or 18")]
+        public int Year { get; set; }
+
+        [Required]
+        [MaxLength(50, ErrorMessage ="Max 50 characters")]
+        public string Make { get; set; }
+
+        [Required]
+        [MaxLength(50, ErrorMessage ="Max 50 characters")]
+        public string Model { get; set; }
+
+        [Required]
+        public double Displacement { get; set; }
+
+        [Required]
+        public bool IsAutomatic { get; set; }
+
+        [Required]
+        public double Odometer { get; set; }
+
+        //public virtual List<FuelUp> 
+        //public virtual List<Maintenance>
+    }
+}
