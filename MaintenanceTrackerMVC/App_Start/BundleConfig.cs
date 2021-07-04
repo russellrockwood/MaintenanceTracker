e63ae8c7c29d84aadd8134c9f5546e75b7b28359
Russell Rockwood <russell.rockwood@gmail.com>
2021-07-04 14:58:13 -0500

    Updated Vehicle Details: checkbox to string, New bootstrap theme

--- MaintenanceTrackerMVC/App_Start/BundleConfig.cs
+++ MaintenanceTrackerMVC/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace MaintenanceTrackerMVC
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-cerulean.css",
                       "~/Content/site.css"));
         }
     }
